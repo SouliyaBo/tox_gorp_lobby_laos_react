a61e6f6539ab5dcc2b186064796adfb85f4c2725
29xaiyavong@gmail.com <29xaiyavong@gmail.com>
2024-04-04 03:23:47 +0700

    login and register

--- src/constant/index.jsx
+++ src/constant/index.jsx
@@ -1,8 +1,16 @@
 module.exports = {
+    SERVER_URL: "https://2ov8dxycl0.execute-api.ap-southeast-1.amazonaws.com/api/v1",
+
     PAGE_LOGIN: "/",
     PAGE_LOGIN_MOBILE: "/login-mobile",
     PAGE_REGISTER_STEP1: "/register-step1",
     PAGE_REGISTER_STEP2: "/register-step2",
     HOME: "/",
+
+    AGEN_CODE: "AG019",
+    
+
+    LOGIN_TOKEN_DATA:"TOX_LOBBY_LAOS_LOGIN_GOLD_TOKEN_DATA",
+    LOGIN_USER_DATA:"TOX_LOBBY_LAOS_LOGIN_GOLD_USER_DATA",
 
 };
